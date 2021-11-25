97895dc42b708e921818adca404ba85cf3cef40d
Francisco J Campos-Laborie <39339795+fjcamlab@users.noreply.github.com>
2021-11-25 16:47:48 +0000

    Update msgfplus_alignment.sh

--- Scripts/msgfplus_alignment.sh
+++ Scripts/msgfplus_alignment.sh
@@ -4,22 +4,18 @@
 # Author: FJ Campos-Laborie
 # MS-GF+ Release (v2020.03.14) (14 March 2020)
 
-
-
 ## Data folder
-DATA=~/EXTERNAL/fjc38/Project_SoniaMelo
-
+DATA=./
 
 ##### MSGF+ search
 ## See tutorial in https://msgfplus.github.io/msgfplus/MSGFPlus.html
-MSGF=/home/fjc38/Software/msgfplus/MSGFPlus.jar
+MSGF=msgfplus/MSGFPlus.jar
 
-cat /home/fjc38/DB/Contaminants/crap.fasta /home/fjc38/DB/EMBLEBI/UP000005640_9606.fasta > /home/fjc38/DB/EMBLEBI/combined_9606_crap.fasta
-DB=/home/fjc38/DB/EMBLEBI/combined_9606_crap.fasta 
-
+cat crap.fasta UP000005640_9606.fasta > combined_9606_crap.fasta
+DB=combined_9606_crap.fasta 
 
 ## create a configuration file (same for all files)
-CONF=/home/fjc38/MS/SoniaMelo_TMT/Qual_conf_file.txt
+CONF=Qual_conf_file.txt
 
 # Search Parameters:
 #         PrecursorMassTolerance: 25.0 ppm
@@ -59,4 +55,3 @@ for file in $DATA/processed/*.mgf; do
 done
 
 mv *.mzid $DATA/aligned
-
